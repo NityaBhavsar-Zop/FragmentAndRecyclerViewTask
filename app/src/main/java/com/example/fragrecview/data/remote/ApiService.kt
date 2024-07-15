cccb4ab67577c2a396b8f25acf519b8df1d2ab26
NityaBhavsar-Zop <nitya.bhavsar@zopsmart.com>
2024-07-15 16:43:46 +0530

    #8 added response for user posts and updated ApiService to get user posts

--- app/src/main/java/com/example/fragrecview/data/remote/ApiService.kt
+++ app/src/main/java/com/example/fragrecview/data/remote/ApiService.kt
@@ -1,10 +1,14 @@
 package com.example.fragrecview.data.remote
 
 import com.example.fragrecview.data.remote.response.UserDetailsList
+import com.example.fragrecview.data.remote.response.UserPostsList
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("users")
     fun getUsers(): Call<List<UserDetailsList>>
+
+    @GET("photos")
+    fun getPhotos(): Call<List<UserPostsList>>
 }
---
+++ app/src/main/java/com/example/fragrecview/data/remote/response/UserPostsList.kt
@@ -0,0 +1,8 @@
+package com.example.fragrecview.data.remote.response
+
+import android.media.Image
+
+data class UserPostsList(
+    val title: String,
+    val thumbnailUrl: Image
+)
