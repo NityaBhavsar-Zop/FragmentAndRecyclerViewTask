7a89306e1fa3fc915dd0d24061bc53daaac2caa9
NityaBhavsar-Zop <nitya.bhavsar@zopsmart.com>
2024-07-19 13:00:56 +0530

    Space removed

--- app/src/main/java/com/example/fragrecview/ui/userdetails/viewmodel/UserDetailsViewModel.kt
+++ app/src/main/java/com/example/fragrecview/ui/userdetails/viewmodel/UserDetailsViewModel.kt
@@ -5,7 +5,6 @@ import com.example.fragrecview.data.local.User
 import com.example.fragrecview.data.local.UserDao
 
 class UserDetailsViewModel(private val userDao: UserDao) : ViewModel() {
-
     fun getUsers(): List<User> {
         return userDao.getAll()
     }
